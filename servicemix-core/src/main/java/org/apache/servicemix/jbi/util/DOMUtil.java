41fbd8951e8b9afdc18e67b0fe63350e482da30d
Guillaume Nodet <gnodet@apache.org>
2006-02-01 11:40:32 +0000

    DomUtil.getElementText does not handle CDATA
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@374052 13f79535-47bb-0310-9956-ffa450edef68

--- servicemix-core/src/main/java/org/apache/servicemix/jbi/util/DOMUtil.java
+++ servicemix-core/src/main/java/org/apache/servicemix/jbi/util/DOMUtil.java
@@ -49,7 +49,7 @@ public class DOMUtil {
         NodeList nodeList = element.getChildNodes();
         for (int i = 0, size = nodeList.getLength(); i < size; i++) {
             Node node = nodeList.item(i);
-            if (node.getNodeType() == Node.TEXT_NODE) {
+            if (node.getNodeType() == Node.TEXT_NODE || node.getNodeType() == Node.CDATA_SECTION_NODE) {
                 buffer.append(node.getNodeValue());
             }
         }
