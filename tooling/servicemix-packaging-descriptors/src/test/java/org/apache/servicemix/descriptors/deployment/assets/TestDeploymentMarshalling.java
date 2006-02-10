9d83dd80ce50a689a40495883819dd8a53deb7f8
Philip James Dodds <pdodds@apache.org>
2006-02-10 19:11:56 +0000

    Updated the packaging profile and added the rest of the packaging engine to become more flexible, also removed the service name and switched to using 'consumes'/'provides' connections in the assets to get the service names etc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@376799 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/servicemix-packaging-descriptors/src/test/java/org/apache/servicemix/descriptors/deployment/assets/TestDeploymentMarshalling.java
+++ tooling/servicemix-packaging-descriptors/src/test/java/org/apache/servicemix/descriptors/deployment/assets/TestDeploymentMarshalling.java
@@ -6,7 +6,8 @@ import javax.xml.bind.Unmarshaller;
 
 import junit.framework.TestCase;
 
-import org.apache.servicemix.descriptors.deployment.assets.Components.Component;
+import org.apache.servicemix.descriptors.packaging.assets.Components;
+import org.apache.servicemix.descriptors.packaging.assets.Components.Component;
 
 public class TestDeploymentMarshalling extends TestCase {
 
@@ -20,11 +21,12 @@ public class TestDeploymentMarshalling extends TestCase {
 					.getClassLoader().getResourceAsStream("components.xml"));
 			System.out.println("Components :" + components);
 			assertNotNull(components.getComponent());
-			assertEquals(1,components.getComponent().size());
+			assertEquals(1, components.getComponent().size());
 			Component component = components.getComponent().get(0);
-			assertEquals("servicemix-lwcontainer",component.getName());
-			assertNotNull(component.getAssets().getDeploymentAssistants());
+			assertEquals("servicemix-lwcontainer", component.getName());
+			assertNotNull(component.getAssets().getEngine());
 		} catch (JAXBException e) {
+			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
