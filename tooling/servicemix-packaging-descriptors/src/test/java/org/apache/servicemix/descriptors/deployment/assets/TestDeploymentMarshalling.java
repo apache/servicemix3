2364a3c85cd3a25e3e45f5e2286021b445194940
Philip James Dodds <pdodds@apache.org>
2006-02-11 21:33:42 +0000

    Added testcase for JBI.xml parsing as well as fixing up the preferences view for registering JBI components
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@377063 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/servicemix-packaging-descriptors/src/test/java/org/apache/servicemix/descriptors/deployment/assets/TestDeploymentMarshalling.java
+++ tooling/servicemix-packaging-descriptors/src/test/java/org/apache/servicemix/descriptors/deployment/assets/TestDeploymentMarshalling.java
@@ -6,8 +6,11 @@ import javax.xml.bind.Unmarshaller;
 
 import junit.framework.TestCase;
 
+import org.apache.servicemix.descriptors.jbi.Jbi;
+import org.apache.servicemix.descriptors.packaging.assets.Assets;
 import org.apache.servicemix.descriptors.packaging.assets.Components;
 import org.apache.servicemix.descriptors.packaging.assets.Components.Component;
+import org.w3c.dom.Element;
 
 public class TestDeploymentMarshalling extends TestCase {
 
@@ -18,13 +21,45 @@ public class TestDeploymentMarshalling extends TestCase {
 					.getName());
 			Unmarshaller m = context.createUnmarshaller();
 			Components components = (Components) m.unmarshal(getClass()
-					.getClassLoader().getResourceAsStream("components.xml"));
-			System.out.println("Components :" + components);
+					.getClassLoader().getResourceAsStream("components.xml"));			
 			assertNotNull(components.getComponent());
 			assertEquals(1, components.getComponent().size());
 			Component component = components.getComponent().get(0);
 			assertEquals("servicemix-lwcontainer", component.getName());
 			assertNotNull(component.getAssets().getEngine());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
+	public void testJbiXml() {
+		JAXBContext context;
+		try {
+			context = JAXBContext.newInstance(Jbi.class.getPackage().getName());
+			Unmarshaller m = context.createUnmarshaller();
+			Jbi jbi = (Jbi) m.unmarshal(getClass().getClassLoader()
+					.getResourceAsStream("jbi.xml"));
+			assertNotNull(jbi.getComponent());
+			assertEquals("servicemix-lwcontainer", jbi.getComponent()
+					.getIdentification().getName());
+			assertEquals("service-engine", jbi.getComponent().getType());
+			assertEquals("LightWeight Container", jbi.getComponent()
+					.getIdentification().getDescription());
+			Assets assets = null;
+			for (Element element : jbi.getComponent().getAnyOrAny()) {				
+				if (("http://servicemix.apache.org/component/packaging"
+						.equals(element.getNamespaceURI()))
+						&& ("assets".equals(element.getLocalName()))) {
+					context = JAXBContext.newInstance(Components.class
+							.getPackage().getName());
+					Unmarshaller m2 = context.createUnmarshaller();
+					assets = (Assets) m2.unmarshal(element);
+				}
+			}
+
+			assertNotNull(assets);
+			assertNotNull(assets.getEngine());
 		} catch (JAXBException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
