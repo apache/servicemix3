3d17ea60d5143d6d5539951f78ffe2243fa8d0b5
Guillaume Nodet <gnodet@apache.org>
2006-03-20 08:38:51 +0000

    Remove unneeded directory Reformat ScaEndpoint
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@387164 13f79535-47bb-0310-9956-ffa450edef68

--- servicemix-sca/src/main/java/org/apache/servicemix/sca/ScaEndpoint.java
+++ servicemix-sca/src/main/java/org/apache/servicemix/sca/ScaEndpoint.java
@@ -37,7 +37,6 @@ import org.apache.servicemix.common.Endpoint;
 import org.apache.servicemix.common.ExchangeProcessor;
 import org.apache.servicemix.jbi.jaxp.StringSource;
 import org.apache.tuscany.core.context.EntryPointContext;
-import org.apache.tuscany.core.invocation.spi.ProxyFactory;
 import org.apache.tuscany.model.assembly.ConfiguredReference;
 import org.apache.tuscany.model.assembly.ConfiguredService;
 import org.apache.tuscany.model.assembly.EntryPoint;
@@ -46,28 +45,30 @@ import org.apache.tuscany.model.assembly.EntryPoint;
  * 
  * @author gnodet
  * @version $Revision: 366467 $
- * @org.apache.xbean.XBean element="endpoint"
- *                  description="A sca endpoint"
+ * @org.apache.xbean.XBean element="endpoint" description="A sca endpoint"
  * 
  */
 public class ScaEndpoint extends Endpoint implements ExchangeProcessor {
 
     protected ServiceEndpoint activated;
+
     protected EntryPoint entryPoint;
-    protected Object proxy;
+
     protected Map<Class, Method> methodMap;
+
     protected JAXBContext jaxbContext;
+
     protected DeliveryChannel channel;
-	
-	public ScaEndpoint(EntryPoint entryPoint) {
-		this.entryPoint = entryPoint;
-	}
 
-	public Role getRole() {
-		return Role.PROVIDER;
-	}
+    public ScaEndpoint(EntryPoint entryPoint) {
+        this.entryPoint = entryPoint;
+    }
 
-	public void activate() throws Exception {
+    public Role getRole() {
+        return Role.PROVIDER;
+    }
+
+    public void activate() throws Exception {
         logger = this.serviceUnit.getComponent().getLogger();
         ComponentContext ctx = this.serviceUnit.getComponent().getComponentContext();
         activated = ctx.activateEndpoint(service, endpoint);
@@ -75,53 +76,51 @@ public class ScaEndpoint extends Endpoint implements ExchangeProcessor {
         // Get the target service
         ConfiguredReference referenceValue = entryPoint.getConfiguredReference();
         ConfiguredService targetServiceEndpoint = referenceValue.getTargetConfiguredServices().get(0);
-        // Create a proxy
-        ProxyFactory proxyFactory = (ProxyFactory) targetServiceEndpoint.getProxyFactory();
-        proxy = proxyFactory.createProxy();
         // Get the business interface
         Class serviceInterface = targetServiceEndpoint.getService().getServiceContract().getInterface();
         List<Class> classes = new ArrayList<Class>();
         methodMap = new HashMap<Class, Method>();
         for (Method mth : serviceInterface.getMethods()) {
-    		Class[] params = mth.getParameterTypes();
-    		if (params.length != 1) {
-    			throw new IllegalStateException("Supports only methods with one parameter");
-    		}
-    		methodMap.put(params[0], mth);
-    		classes.add(mth.getReturnType());
-    		classes.add(params[0]);
+            Class[] params = mth.getParameterTypes();
+            if (params.length != 1) {
+                throw new IllegalStateException("Supports only methods with one parameter");
+            }
+            methodMap.put(params[0], mth);
+            classes.add(mth.getReturnType());
+            classes.add(params[0]);
         }
         jaxbContext = JAXBContext.newInstance(classes.toArray(new Class[0]));
-	}
+    }
 
-	public void deactivate() throws Exception {
+    public void deactivate() throws Exception {
         ServiceEndpoint ep = activated;
         activated = null;
         ComponentContext ctx = this.serviceUnit.getComponent().getComponentContext();
         ctx.deactivateEndpoint(ep);
-	}
+    }
 
-	public ExchangeProcessor getProcessor() {
-		return this;
-	}
+    public ExchangeProcessor getProcessor() {
+        return this;
+    }
 
-	public void process(MessageExchange exchange) throws Exception {
-		if (exchange.getStatus() == ExchangeStatus.DONE) {
-			return;
-		} else if (exchange.getStatus() == ExchangeStatus.ERROR) {
-			return;
-		}
-		Object input = jaxbContext.createUnmarshaller().unmarshal(exchange.getMessage("in").getContent());
-		Method method = methodMap.get(input.getClass());
-		if (method ==  null) {
-			throw new IllegalStateException("Could not determine invoked web method");
-		}
-		boolean oneWay = method.getReturnType() == null;
-		Object output;
-		try {
-            EntryPointContext entryPointContext = (EntryPointContext) ((ScaServiceUnit) serviceUnit).getTuscanyRuntime().getModuleContext().getContext(entryPoint.getName());
+    public void process(MessageExchange exchange) throws Exception {
+        if (exchange.getStatus() == ExchangeStatus.DONE) {
+            return;
+        } else if (exchange.getStatus() == ExchangeStatus.ERROR) {
+            return;
+        }
+        Object input = jaxbContext.createUnmarshaller().unmarshal(exchange.getMessage("in").getContent());
+        Method method = methodMap.get(input.getClass());
+        if (method == null) {
+            throw new IllegalStateException("Could not determine invoked web method");
+        }
+        boolean oneWay = method.getReturnType() == null;
+        Object output;
+        try {
+            EntryPointContext entryPointContext = (EntryPointContext) ((ScaServiceUnit) serviceUnit)
+                    .getTuscanyRuntime().getModuleContext().getContext(entryPoint.getName());
             InvocationHandler handler = (InvocationHandler) entryPointContext.getImplementationInstance();
-			output = handler.invoke(null, method, new Object[] { input });
+            output = handler.invoke(null, method, new Object[] { input });
         } catch (UndeclaredThrowableException e) {
             throw e;
         } catch (RuntimeException e) {
@@ -130,26 +129,26 @@ public class ScaEndpoint extends Endpoint implements ExchangeProcessor {
             throw e;
         } catch (Exception e) {
             throw e;
-		} catch (Throwable e) {
-			throw new RuntimeException(e);
-		}
-		if (oneWay) {
-			exchange.setStatus(ExchangeStatus.DONE);
-			channel.send(exchange);
-		} else {
-			NormalizedMessage msg = exchange.createMessage();
-			exchange.setMessage(msg, "out");
-			StringWriter writer = new StringWriter();
-			jaxbContext.createMarshaller().marshal(output, writer);
-			msg.setContent(new StringSource(writer.toString()));
-			channel.send(exchange);
-		}
-	}
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+        if (oneWay) {
+            exchange.setStatus(ExchangeStatus.DONE);
+            channel.send(exchange);
+        } else {
+            NormalizedMessage msg = exchange.createMessage();
+            exchange.setMessage(msg, "out");
+            StringWriter writer = new StringWriter();
+            jaxbContext.createMarshaller().marshal(output, writer);
+            msg.setContent(new StringSource(writer.toString()));
+            channel.send(exchange);
+        }
+    }
 
-	public void start() throws Exception {
-	}
+    public void start() throws Exception {
+    }
 
-	public void stop() throws Exception {
-	}
+    public void stop() throws Exception {
+    }
 
 }
