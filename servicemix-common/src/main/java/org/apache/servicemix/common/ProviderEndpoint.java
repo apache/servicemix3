c86146b70636631df4aecf5c00df0e3db6c941b7
James Strachan <jstrachan@apache.org>
2006-10-02 19:41:54 +0000

    minor refactor so that the saxon and file components can reuse more logic from the ProviderEndpoint - making it easier for derived endpoints to just implement specific methods (rather like the default methods on the Servlet classes to allow you to overload a GET or POST etc). Also added a test case for servicemix-file which shows the file marshaler / xpath expressions being used
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@452180 13f79535-47bb-0310-9956-ffa450edef68

--- servicemix-common/src/main/java/org/apache/servicemix/common/ProviderEndpoint.java
+++ servicemix-common/src/main/java/org/apache/servicemix/common/ProviderEndpoint.java
@@ -6,8 +6,12 @@ import javax.jbi.messaging.ExchangeStatus;
 import javax.jbi.messaging.MessageExchange;
 import javax.jbi.messaging.MessageExchangeFactory;
 import javax.jbi.messaging.MessagingException;
+import javax.jbi.messaging.NormalizedMessage;
+import javax.jbi.messaging.InOnly;
+import javax.jbi.messaging.RobustInOnly;
 import javax.jbi.messaging.MessageExchange.Role;
 import javax.jbi.servicedesc.ServiceEndpoint;
+import javax.jbi.JBIException;
 
 public abstract class ProviderEndpoint extends Endpoint implements ExchangeProcessor {
 
@@ -75,4 +79,66 @@ public abstract class ProviderEndpoint extends Endpoint implements ExchangeProce
     public void stop() throws Exception {
     }
 
+
+    /**
+     * A default implementation of the message processor which checks the status of the exchange
+     * and if its valid will dispatch to either {@link #processInOnly(MessageExchange,NormalizedMessage)} for
+     * an {@link InOnly} or {@link RobustInOnly} message exchange otherwise the
+     * {@link #processInOut(MessageExchange,NormalizedMessage,NormalizedMessage)}
+     * method will be invoked
+     *
+     * @param exchange the message exchange
+     * @throws Exception
+     */
+    public void process(MessageExchange exchange) throws Exception {
+        // The component acts as a provider, this means that another component has requested our service
+        // As this exchange is active, this is either an in or a fault (out are sent by this component)
+        if (exchange.getRole() == Role.PROVIDER) {
+            // Exchange is finished
+            if (exchange.getStatus() == ExchangeStatus.DONE) {
+                return;
+            // Exchange has been aborted with an exception
+            } else if (exchange.getStatus() == ExchangeStatus.ERROR) {
+                return;
+            // Exchange is active
+            } else {
+                // In message
+                NormalizedMessage in = exchange.getMessage("in");
+                if (in != null) {
+                    if (exchange instanceof InOnly || exchange instanceof RobustInOnly) {
+                        processInOnly(exchange, in);
+                    }
+                    else {
+                        NormalizedMessage out = exchange.getMessage("out");
+                        if (out == null) {
+                            out = exchange.createMessage();
+                            exchange.setMessage(out, "out");
+                        }
+                        processInOut(exchange, in, out);
+                    }
+                    send(exchange);
+
+                // Fault message
+                } else if (exchange.getFault() != null) {
+                    done(exchange);
+                // This is not compliant with the default MEPs
+                } else {
+                    throw new IllegalStateException("Provider exchange is ACTIVE, but no in or fault is provided");
+                }
+            }
+        // Unsupported role: this should never happen has we never create exchanges
+        } else {
+            throw new IllegalStateException("Unsupported role: " + exchange.getRole());
+        }
+    }
+
+
+    protected void processInOnly(MessageExchange exchange, NormalizedMessage in) throws Exception {
+        throw new UnsupportedOperationException("Unsupported MEP: " + exchange.getPattern());
+    }
+
+    protected void processInOut(MessageExchange exchange, NormalizedMessage in, NormalizedMessage out) throws Exception {
+        throw new UnsupportedOperationException("Unsupported MEP: " + exchange.getPattern());
+    }
+    
 }
