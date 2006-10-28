c033526c4a8a17f0d77561874922eb20825d54cd
Guillaume Nodet <gnodet@apache.org>
2006-10-28 19:52:07 +0000

    Use spring 2.0 xsds, fix css, externalize jmx properties. Allow the web app to start even if jdbc auditor or dot service are not configured
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@468730 13f79535-47bb-0310-9956-ffa450edef68

--- servicemix-web-console/src/main/java/org/apache/servicemix/web/Auditor.java
+++ servicemix-web-console/src/main/java/org/apache/servicemix/web/Auditor.java
@@ -1,28 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicemix.web;
 
-import java.io.IOException;
 import java.net.URI;
 import java.text.DateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.jbi.messaging.MessageExchange;
-import javax.jbi.messaging.MessagingException;
 import javax.jbi.messaging.NormalizedMessage;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 
 import org.apache.servicemix.JbiConstants;
 import org.apache.servicemix.jbi.audit.AuditorException;
 import org.apache.servicemix.jbi.audit.AuditorMBean;
 import org.apache.servicemix.jbi.jaxp.SourceTransformer;
 import org.apache.servicemix.jbi.messaging.MessageExchangeSupport;
-import org.xml.sax.SAXException;
 
 public class Auditor {
 
@@ -52,7 +61,6 @@ public class Auditor {
     
     public int getCount() throws AuditorException {
         int count = mbean.getExchangeCount();
-        System.err.println(count);
         return count;
     }
 
@@ -176,6 +184,7 @@ public class Auditor {
         public MessageInfo(NormalizedMessage message) {
             this.message = message;
         }
+        @SuppressWarnings("unchecked")
         public String getProperties() {
             StringBuilder sb = new StringBuilder();
             for (String name : (Set<String>) message.getPropertyNames()) {
