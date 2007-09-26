6010f86884f26688c62719a376f7534caf7af837
Guillaume Nodet <gnodet@apache.org>
2007-09-26 12:05:21 +0000

    Fix test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/trunk@579628 13f79535-47bb-0310-9956-ffa450edef68

--- common/servicemix-soap/src/test/java/org/apache/servicemix/soap/handlers/addressing/AddressingHandlerTest.java
+++ common/servicemix-soap/src/test/java/org/apache/servicemix/soap/handlers/addressing/AddressingHandlerTest.java
@@ -1,7 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.servicemix.soap.handlers.addressing;
 
 import javax.xml.namespace.QName;
 
+import org.apache.servicemix.jbi.util.WSAddressingConstants;
 import org.apache.servicemix.soap.Context;
 import org.apache.servicemix.soap.marshalers.SoapMessage;
 import org.w3c.dom.DocumentFragment;
@@ -22,7 +39,7 @@ public class AddressingHandlerTest extends TestCase {
 	}
 	
 	public void testCreateHeader() throws Exception {
-		QName messageIdQN = new QName(AddressingHandler.WSA_NAMESPACE_200408, AddressingHandler.EL_MESSAGE_ID, AddressingHandler.WSA_PREFIX);
+		QName messageIdQN = new QName(WSAddressingConstants.WSA_NAMESPACE_200408, WSAddressingConstants.EL_MESSAGE_ID, WSAddressingConstants.WSA_PREFIX);
 		String messageId = "uuid:1234567890";
 		DocumentFragment wsaMessageId = this.handler.createHeader(messageIdQN, messageId);
 
@@ -33,8 +50,8 @@ public class AddressingHandlerTest extends TestCase {
 	
 	public void testWSAEmptyPrefix() throws Exception {
 		// setup
-		QName messageIdQN = new QName(AddressingHandler.WSA_NAMESPACE_200408, AddressingHandler.EL_MESSAGE_ID, "");
-		QName relatesToQN = new QName(AddressingHandler.WSA_NAMESPACE_200408, AddressingHandler.EL_RELATES_TO, "");
+		QName messageIdQN = new QName(WSAddressingConstants.WSA_NAMESPACE_200408, WSAddressingConstants.EL_MESSAGE_ID, "");
+		QName relatesToQN = new QName(WSAddressingConstants.WSA_NAMESPACE_200408, WSAddressingConstants.EL_RELATES_TO, "");
 
 		
 		// create messages and add them to the context
@@ -60,8 +77,8 @@ public class AddressingHandlerTest extends TestCase {
 	
 	public void testWSAPrefix() throws Exception {
 		// setup
-		QName messageIdQN = new QName(AddressingHandler.WSA_NAMESPACE_200408, AddressingHandler.EL_MESSAGE_ID, AddressingHandler.WSA_PREFIX);
-		QName relatesToQN = new QName(AddressingHandler.WSA_NAMESPACE_200408, AddressingHandler.EL_RELATES_TO, AddressingHandler.WSA_PREFIX);
+		QName messageIdQN = new QName(WSAddressingConstants.WSA_NAMESPACE_200408, WSAddressingConstants.EL_MESSAGE_ID, WSAddressingConstants.WSA_PREFIX);
+		QName relatesToQN = new QName(WSAddressingConstants.WSA_NAMESPACE_200408, WSAddressingConstants.EL_RELATES_TO, WSAddressingConstants.WSA_PREFIX);
 
 		
 		// create messages and add them to the context
