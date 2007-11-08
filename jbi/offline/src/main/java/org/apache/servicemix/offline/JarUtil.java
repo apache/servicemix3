7b8cd55c9e7779b74e02d0509926bf21d4f886ae
Bruce Snyder <bsnyder@apache.org>
2007-11-08 23:39:12 +0000

    Changed the for loops to the new syntax.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/servicemix/branches/servicemix-4.0@593358 13f79535-47bb-0310-9956-ffa450edef68

--- jbi/offline/src/main/java/org/apache/servicemix/offline/JarUtil.java
+++ jbi/offline/src/main/java/org/apache/servicemix/offline/JarUtil.java
@@ -1,11 +1,7 @@
 package org.apache.servicemix.offline;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.jar.Attributes;
-import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 import org.springframework.core.io.Resource;
@@ -24,15 +20,15 @@ public class JarUtil {
         StringBuffer content = new StringBuffer();
         final String indent = " "; 
         
-        Map entries = manifest.getEntries();
+        Map<String, Attributes> entries = manifest.getEntries();
         
-        for (Iterator iter0 = (Iterator) entries.keySet().iterator(); iter0.hasNext();) {
-            String entryName = (String) iter0.next();
+        for (String string : entries.keySet()) {
+            String entryName = (String) string;
             content.append(entryName);
             Attributes attrs = (Attributes) entries.get(entryName);
             
-            for (Iterator iter1 = (Iterator) attrs.keySet().iterator(); iter1.hasNext();) {
-                Attributes.Name name = (Attributes.Name) iter1.next();
+            for (Object element : attrs.keySet()) {
+                Attributes.Name name = (Attributes.Name) element;
                 String attr = attrs.getValue(name);
                 content.append(indent + attr);
             }
