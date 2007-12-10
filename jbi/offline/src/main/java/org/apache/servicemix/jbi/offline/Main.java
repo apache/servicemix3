4166d3e155d0c862719913c04ce176d407db6770
Freeman Yue Fang <ffang@apache.org>
2007-12-10 03:27:40 +0000

    [SM-1090] apply patch on behalf of Jeff Yu
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/branches/servicemix-4.0@602775 13f79535-47bb-0310-9956-ffa450edef68

--- jbi/offline/src/main/java/org/apache/servicemix/jbi/offline/Main.java
+++ jbi/offline/src/main/java/org/apache/servicemix/jbi/offline/Main.java
@@ -18,11 +18,11 @@ package org.apache.servicemix.jbi.offline;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Map;
 import java.util.jar.Attributes;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -36,41 +36,65 @@ import org.apache.servicemix.jbi.deployer.descriptor.DescriptorFactory;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        new Main().run(args);
+    	new Main().run(args);       
+    }
+    
+    public void run(String... args) throws Exception{
+    	
+    	if (args != null && args.length == 1 && "-help".equals(args[0])) {
+    		showUsage();
+    	} else if ( args != null && args.length == 2) {
+    		Options options = new Options(args);
+    		run(options);
+    	} else {
+    		System.out.println("Bad Usage!");
+    		showUsage();
+    	}
     }
 
-    public void run(String... args) throws Exception {
-        File f = new File(args[0]);
+    public void run(Options options) throws Exception {
+        File f = new File(options.getOrginalFile());
         if (!f.exists()) {
             throw new IllegalStateException("File " + f.toString() + " does not exists.");
         }
-        JarFile jar = new JarFile(args[0]);
+        JarFile jar = new JarFile(options.getOrginalFile());
         Manifest m = jar.getManifest();
         JarEntry entry = jar.getJarEntry("META-INF/jbi.xml");
         InputStream is = jar.getInputStream(entry);
         Descriptor desc = DescriptorFactory.buildDescriptor(is);
-
+        
+        String version = m.getMainAttributes().getValue("Implementation-Version");
+        String name = m.getMainAttributes().getValue("Implementation-Title");
+        
         if (desc.getComponent() != null) {
-            String name = desc.getComponent().getIdentification().getName();
-            String version = m.getMainAttributes().getValue("Implementation-Version");
-            m.getMainAttributes().put(new Attributes.Name("Bundle-SymbolicName"), name);
-            m.getMainAttributes().put(new Attributes.Name("Bundle-Version"), version);
+            name = desc.getComponent().getIdentification().getName();       
+        } else if (desc.getSharedLibrary() != null) {
+        	name = desc.getSharedLibrary().getIdentification().getName();
+        } else if (desc.getServiceAssembly() != null) {
+        	name = desc.getServiceAssembly().getIdentification().getName();        	
         }
+        
+        m.getMainAttributes().put(new Attributes.Name("Bundle-SymbolicName"), name);
+        m.getMainAttributes().put(new Attributes.Name("Bundle-Version"), version);
 
-        JarInputStream jis = new JarInputStream(new FileInputStream(args[0]));
-        JarOutputStream jos = new JarOutputStream(new FileOutputStream(args[1]), m);
+        generateBundle(options, m);
+        
+    }
 
-        byte[] buffer = new byte[8192];
-        entry = jis.getNextJarEntry();
+	private void generateBundle(Options options, Manifest m) throws IOException, FileNotFoundException {
+		JarInputStream jis = new JarInputStream(new FileInputStream(options.getOrginalFile()));
+        JarOutputStream jos = new JarOutputStream(new FileOutputStream(options.getDestFile()), m);
+        
+        JarEntry entry = jis.getNextJarEntry();
         while (entry != null) {
-            jos.putNextEntry(entry);
+    	    jos.putNextEntry(entry);
             copyInputStream(jis, jos);
             jos.closeEntry();
             entry = jis.getNextJarEntry();
         }
         jos.close();
         jis.close();
-    }
+	}
 
     protected static void copyInputStream(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[4096];
@@ -78,5 +102,31 @@ public class Main {
         while ((len = in.read(buffer)) >= 0) {
             out.write(buffer, 0, len);
         }
+    }
+    
+    protected static void showUsage() {
+    	System.out.println("usage: convert [jar/zip file (Component, SL, SA) ] [OSGied jar/zip file (Component, SL, SA)]");
+    }
+    
+    
+    public static class Options {
+    	
+    	private String orginalFile ;
+    	
+    	private String destFile;
+    	
+    	public Options(String... args) {
+    		this.orginalFile = args[0];
+    		this.destFile = args[1];
+    	}
+    	
+		public String getOrginalFile() {
+			return orginalFile;
+		}
+		public String getDestFile() {
+			return destFile;
+		}
+    	
+    	
     }
 }
