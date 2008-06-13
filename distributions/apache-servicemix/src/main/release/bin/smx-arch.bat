6bd89401ac79de8e30ece9b4febb23968d2dc3eb
Lars Heinemann <lhein@apache.org>
2008-06-13 22:44:57 +0000

    - added new archetype for validation-su to smx-arch scripts
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx3/trunk@667687 13f79535-47bb-0310-9956-ffa450edef68

--- distributions/apache-servicemix/src/main/release/bin/smx-arch.bat
+++ distributions/apache-servicemix/src/main/release/bin/smx-arch.bat
@@ -51,7 +51,7 @@ echo                     ftp-poller, ftp-sender,
 echo                     jsr181-annotated, jsr181-wsdl-first, mail, 
 echo                     saxon-xquery, saxon-xslt, osworkflow,
 echo                     eip, lwcontainer, bean, ode, camel, scripting,
-echo                     cxf-se, cxf-se-wsdl-first, cxf-bc
+echo                     cxf-se, cxf-se-wsdl-first, cxf-bc, validation
 echo Optional arguments:
 echo   -DgroupId=xxxx
 echo   -DartifactId=xxxx
