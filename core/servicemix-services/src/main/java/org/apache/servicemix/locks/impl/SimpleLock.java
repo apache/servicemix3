9c693b82da75d5f8c07ff902e938761bf5de1c66
Guillaume Nodet <gnodet@apache.org>
2008-07-08 14:11:29 +0000

    SM-1441: Use a real lock implementation, because the lock() method is heavily used by servicemix-eip
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx3/trunk@674833 13f79535-47bb-0310-9956-ffa450edef68

--- core/servicemix-services/src/main/java/org/apache/servicemix/locks/impl/SimpleLock.java
+++ core/servicemix-services/src/main/java/org/apache/servicemix/locks/impl/SimpleLock.java
@@ -20,60 +20,84 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * @author lhein
  */
-public class SimpleLock implements Lock {
+public class SimpleLock implements Lock, Serializable {
 
-    private AtomicBoolean lock;
+    // Our internal helper class
+    private static class Sync extends AbstractQueuedSynchronizer {
+        // Report whether in locked state
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
 
-    /**
-     * default constructor
-     */
-    public SimpleLock() {
-        this.lock = new AtomicBoolean(false);
+        // Acquire the lock if state is zero
+        public boolean tryAcquire(int acquires) {
+            assert acquires == 1; // Otherwise unused
+            return compareAndSetState(0, 1);
+        }
+
+        // Release the lock by setting state to zero
+        protected boolean tryRelease(int releases) {
+            assert releases == 1; // Otherwise unused
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setState(0);
+            return true;
+        }
+
+        // Provide a Condition
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+
+        // Deserialize properly
+        private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+            s.defaultReadObject();
+            setState(0); // reset to unlocked state
+        }
     }
-    
-    /* (non-Javadoc)
-     * @see java.util.concurrent.locks.Lock#lock()
-     */
+
+    // The sync object does all the hard work. We just forward to it.
+    private final Sync sync = new Sync();
+
     public void lock() {
-        throw new UnsupportedOperationException();
+        sync.acquire(1);
     }
 
-    /* (non-Javadoc)
-     * @see java.util.concurrent.locks.Lock#lockInterruptibly()
-     */
-    public void lockInterruptibly() throws InterruptedException {
-        throw new UnsupportedOperationException();
-    }
-
-    /* (non-Javadoc)
-     * @see java.util.concurrent.locks.Lock#newCondition()
-     */
-    public Condition newCondition() {
-        throw new UnsupportedOperationException();
-    }
-
-    /* (non-Javadoc)
-     * @see java.util.concurrent.locks.Lock#tryLock()
-     */
     public boolean tryLock() {
-        return this.lock.compareAndSet(false, true);
+        return sync.tryAcquire(1);
     }
 
-    /* (non-Javadoc)
-     * @see java.util.concurrent.locks.Lock#tryLock(long, java.util.concurrent.TimeUnit)
-     */
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        throw new UnsupportedOperationException();
-    }
-
-    /* (non-Javadoc)
-     * @see java.util.concurrent.locks.Lock#unlock()
-     */
     public void unlock() {
-        this.lock.compareAndSet(true, false);
+        sync.release(1);
     }
+
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(timeout));
+    }
+
 }
