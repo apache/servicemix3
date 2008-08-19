19f15af0eceb82ffe2aa9f91362f5b4f44bb8e00
Freeman Yue Fang <ffang@apache.org>
2008-08-19 01:35:43 +0000

    [SM-1512]update file component generated reference docs
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx3/branches/servicemix-3.2@686929 13f79535-47bb-0310-9956-ffa450edef68

--- deployables/bindingcomponents/servicemix-file/src/main/java/org/apache/servicemix/file/FileComponent.java
+++ deployables/bindingcomponents/servicemix-file/src/main/java/org/apache/servicemix/file/FileComponent.java
@@ -32,16 +32,26 @@ import org.apache.servicemix.jbi.util.URISupport;
  * A file based component
  *
  * @version $Revision$
- * @org.apache.xbean.XBean element="component" description="File Component"
+ * @org.apache.xbean.XBean element="component" description="a JBI component that interacts with the file system. 
+ *         It hosts endpoints that reads data from and writes data to the file system."
  */
 public class FileComponent extends DefaultComponent {
 
     private FileEndpointType[] endpoints;
 
+    /**
+     * Returns a list of endpoints being managed by the component.
+     */
     public FileEndpointType[] getEndpoints() {
         return endpoints;
     }
 
+    /**
+     * Sets the list of endpoint managed by the component.
+     *
+     * @param endpoints an array of <code>FileEndpointType</code> objects
+     * @org.apache.xbean.Property description="a list of beans defining the endpoints hosted by the component"
+     */
     public void setEndpoints(FileEndpointType[] endpoints) {
         this.endpoints = endpoints;
     }
