208a1f3070e93b82aeabebe624dddb9b1b72cf4c
Gert Vanthienen <gertv@apache.org>
2008-10-24 13:59:48 +0000

    SM-1456: Statistics Service throws NPE on startup.
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx3/branches/servicemix-3.3@707637 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/servicemix-core/src/test/java/org/apache/servicemix/jbi/monitoring/StatisticsServiceTest.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.servicemix.jbi.monitoring;
+
+import javax.jbi.JBIException;
+import javax.jbi.servicedesc.ServiceEndpoint;
+import javax.xml.namespace.QName;
+
+import junit.framework.TestCase;
+
+import org.apache.servicemix.jbi.container.ActivationSpec;
+import org.apache.servicemix.jbi.container.JBIContainer;
+import org.apache.servicemix.jbi.framework.ComponentContextImpl;
+import org.apache.servicemix.jbi.messaging.DeliveryChannelImplTest.TestComponent;
+import org.apache.servicemix.jbi.servicedesc.EndpointSupport;
+
+/**
+ * Test case for {@link StatisticsService}
+ */
+public class StatisticsServiceTest extends TestCase {
+    
+    private static final String COMPONENT = "component";
+    private static final String ENDPOINT = "endpoint";
+    private static final QName SERVICE = new QName("service");
+
+    protected JBIContainer container;
+    private StatisticsService service;
+
+    protected void setUp() throws Exception {
+        // set up a test container instance
+        container = new JBIContainer();
+        container.setEmbedded(true);
+        container.init();
+        container.start();
+        // and a test StatisticsService
+        service = new StatisticsService();
+    }
+    
+    public void testAddEndpointStatsByListener() throws JBIException {
+        // initialize and start the StatisticsService
+        service.init(container);
+        service.start();
+
+        // now register a new endpoint
+        ServiceEndpoint endpoint = registerEndpoint();
+
+        // StatisticsService should know about the endpoint/component through listener callbacks
+        assertNotNull(service.getComponentStats().get(COMPONENT));
+        assertNotNull(service.getEndpointStats().get(EndpointSupport.getUniqueKey(endpoint)));
+    }
+    
+    public void testAddEndpointStatsAtStartup() throws JBIException {
+        // first register the endpoint
+        ServiceEndpoint endpoint = registerEndpoint();
+        
+        // initialize and start the StatisticsService
+        service.init(container);
+        service.start();
+
+        // StatisticsService should have learn about existing endpoints/components at startup
+        assertNotNull(service.getComponentStats().get(COMPONENT));
+        assertNotNull(service.getEndpointStats().get(EndpointSupport.getUniqueKey(endpoint)));
+    }
+
+    private ServiceEndpoint registerEndpoint() throws JBIException {
+        TestComponent component = new TestComponent(SERVICE, ENDPOINT);
+        container.activateComponent(new ActivationSpec(COMPONENT, component));
+        return container.getEndpoint((ComponentContextImpl)component.getContext(), SERVICE, ENDPOINT);
+    }
+
+}
