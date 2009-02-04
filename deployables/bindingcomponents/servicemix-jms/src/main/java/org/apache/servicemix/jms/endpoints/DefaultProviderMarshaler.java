acf5e3e485e986eb2f3ec5befd0a538af9d4e187
Guillaume Nodet <gnodet@apache.org>
2009-02-04 22:08:42 +0000

    SM-1788: The 'Content-Type' property is used in jms messages, but it is not compliant with the jms spec
    git-svn-id: https://svn.apache.org/repos/asf/servicemix/smx3/branches/servicemix-3.2@740913 13f79535-47bb-0310-9956-ffa450edef68

--- deployables/bindingcomponents/servicemix-jms/src/main/java/org/apache/servicemix/jms/endpoints/DefaultProviderMarshaler.java
+++ deployables/bindingcomponents/servicemix-jms/src/main/java/org/apache/servicemix/jms/endpoints/DefaultProviderMarshaler.java
@@ -74,8 +74,10 @@ public class DefaultProviderMarshaler extends AbstractJmsMarshaler implements Jm
         }
         chain.doIntercept(msg);
         TextMessage text = session.createTextMessage(baos.toString());
-        text.setStringProperty(org.apache.servicemix.soap.api.Message.CONTENT_TYPE,
-                               (String) msg.get(org.apache.servicemix.soap.api.Message.CONTENT_TYPE));
+        if (msg.get(org.apache.servicemix.soap.api.Message.CONTENT_TYPE) != null) {
+            text.setStringProperty(CONTENT_TYPE_PROPERTY,
+                                   (String) msg.get(org.apache.servicemix.soap.api.Message.CONTENT_TYPE));
+        }
         if (jmsProperties != null) {
             for (Map.Entry<String, Object> e : jmsProperties.entrySet()) {
                 text.setObjectProperty(e.getKey(), e.getValue());
@@ -97,9 +99,8 @@ public class DefaultProviderMarshaler extends AbstractJmsMarshaler implements Jm
             chain.add(new StaxInInterceptor());
             org.apache.servicemix.soap.api.Message msg = new MessageImpl();
             msg.setContent(InputStream.class, new ByteArrayInputStream(((TextMessage) message).getText().getBytes()));
-            String contentType = message.getStringProperty(org.apache.servicemix.soap.api.Message.CONTENT_TYPE);
-            if (contentType != null) {
-                msg.put(org.apache.servicemix.soap.api.Message.CONTENT_TYPE, contentType);
+            if (message.propertyExists(CONTENT_TYPE_PROPERTY)) {
+                msg.put(org.apache.servicemix.soap.api.Message.CONTENT_TYPE, message.getStringProperty(CONTENT_TYPE_PROPERTY));
             }
             chain.doIntercept(msg);
             XMLStreamReader xmlReader = msg.getContent(XMLStreamReader.class);
